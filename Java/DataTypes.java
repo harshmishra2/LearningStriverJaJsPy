3ea14aee077c3cc9d941b93735f342c2847f9a47
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-03-21 11:49:57 +0530

    Create DataTypes.java

--- Java/DataTypes.java
+++ Java/DataTypes.java
@@ -53,3 +53,5 @@ class Solution {
         else return -1;
     }
 }
+
+// https://www.geeksforgeeks.org/problems/data-type-1666706751/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=data-type
