5bb138628d46b96a7b1409a294f396049b061f9e
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-03-23 18:23:05 +0530

    Create N_Forest.py

---
+++ Python/N_Forest.py
@@ -0,0 +1,11 @@
+// https://www.naukri.com/code360/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
+def nForest(n:int) ->None:
+    # Write your solution here.
+
+    for x in range(n):
+        if(x != 0):
+            print()
+        for y in range(n):
+            print('* ', end='')
+
+    pass
