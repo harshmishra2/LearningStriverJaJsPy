37f00df59c6d8f37c475caaa688e35747d0ca87d
Harsh Mishra <71183727+harshmishra2@users.noreply.github.com>
2025-03-23 16:50:11 +0530

    Create N_forest.java

---
+++ Java/N_forest.java
@@ -0,0 +1,13 @@
+public class N_forest {
+    public static void nForest(int n) {
+        // Write your code here
+
+        for(int i=0; i<n; i++){
+            if(i!=0)    System.out.println("");
+            for(int j=0; j<n; j++){
+                System.out.print("* ");
+            }
+        }
+    }
+}
+https://www.naukri.com/code360/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
